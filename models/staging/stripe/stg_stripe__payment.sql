d3f9b753219ea83f3b72410c6336d1ea0cc0e236
khaririv.aws@gmail.com <khaririv.aws@gmail.com>
2026-03-03 03:16:00 +0000

    TEST files for customers, orders, and payment

--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -13,8 +13,8 @@ renamed as (
         orderid as order_id,
         paymentmethod as payment_method, 
         status as payment_status,
-        amount as payment_amount,
-        created,
+        amount /100  as payment_amount,
+        created as created_at,
         _batched_at
 
     from source
